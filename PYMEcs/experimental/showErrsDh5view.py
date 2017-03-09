920d0a3a6a84047bc286e4f75d76c20f727c889c
Christian Soeller <c.soeller@gmail.com>
2017-03-09 19:56:22 +0000

    dh5view error log viewer overhaul

--- PYMEcs/experimental/showErrsDh5view.py
+++ PYMEcs/experimental/showErrsDh5view.py
@@ -1,55 +1,27 @@
 import wx
-
-# this is a quick hack
-# it needs to be properly wrapped in a frame etc
-# for now a convenient but ugly way to see error messages on OS X
-
-class WindowPopup(wx.PopupWindow):
-   """ Pops up a window to provide description for the selection """
-   def __init__(self, parent, style, content):
-      wx.PopupWindow.__init__(self, parent, style)
-
-      sizer = wx.BoxSizer(wx.VERTICAL)
-      self.st = wx.TextCtrl(self, -1, style = wx.TE_MULTILINE | wx.TE_READONLY, size = (900, 400))
-      self.st.SetValue(content)
-      self.SetSize((900, 400))
-      sizer.Add(self.st, 0, wx.EXPAND)
-      self.SetSizer(sizer)
-      self.Layout()
-      self.Show(True)
-
-
+from  wx.lib.dialogs import ScrolledMessageDialog
 
 class ShowErr:
-    """
+   """
 
-    """
-    def __init__(self, visFr):
-        self.visFr = visFr
-        self.txtwin = None
-        visFr.AddMenuItem('Experimental', 'Show/Update error log', self.OnShowErr)
-        visFr.AddMenuItem('Experimental', 'Hide error log', self.OnHideErr)
+   """
+   def __init__(self, visFr):
+      self.visFr = visFr
+      self.txtwin = None
+      visFr.AddMenuItem('Experimental', 'Errors in scrolled message dialog\tCtrl+E', self.OnErrScrolledDialog)
 
+   def getLogFileName(self):
+      import os
+      pid = int(os.getpid()) - 1
+      return os.path.join('/tmp','dh5view-%d.tmp' % pid)
 
-    def OnShowErr(self, event=None):
-        import os
-        pid = int(os.getpid()) - 1
-
-        with open(os.path.join('/tmp','dh5view-%d.tmp' % pid),"r") as f:
-            self.txt = "\n".join(f.readlines())
-
-        if self.txtwin is None:
-            self.txtwin = WindowPopup(self.visFr,wx.SIMPLE_BORDER, self.txt)
-        else:
-            self.txtwin.st.SetValue(self.txt)
-            self.txtwin.Layout()
-
-        self.txtwin.Show()
-
-    def OnHideErr(self, event=None):
-        if self.txtwin is not None:
-            self.txtwin.Hide()
-
+   def OnErrScrolledDialog(self, event=None):
+      with open(self.getLogFileName(),"r") as f:
+         txt = "\n".join(f.readlines())
+      dlg = ScrolledMessageDialog(self.visFr, txt, "VisGUI Error Output", size=(900,400),
+                                  style=wx.RESIZE_BORDER | wx.DEFAULT_DIALOG_STYLE )
+      dlg.ShowModal()
+      dlg.Destroy()
 
 def Plug(visFr):
     """Plugs this module into the gui"""
