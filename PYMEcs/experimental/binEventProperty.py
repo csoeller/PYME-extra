d04fc2351823a5377be54595aff548c80e55d292
Christian Soeller <c.soeller@gmail.com>
2017-10-22 21:44:55 +0100

    added binning by event property

---
+++ PYMEcs/experimental/binEventProperty.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import logging
+logger = logging.getLogger(__file__)
+
+from traits.api import HasTraits, Str, Int, CStr, List, Enum, Float
+from traitsui.api import View, Item, Group
+from traitsui.menu import OKButton, CancelButton, OKCancelButtons
+
+class propertyChoice(HasTraits):
+    clist = List([])
+    EventProperty = Enum(values='clist')
+    BinByProperty = Enum(values='clist')
+    BinWidth = Float(100.0)
+
+    traits_view = View(Group(Item(name = 'EventProperty'),
+                             Item(name = 'BinByProperty'),
+                             Item('_'),
+                             Item(name = 'BinWidth'),
+                             label = 'Select Properties and Binning',
+                             show_border = True),
+                       buttons = OKCancelButtons)
+
+    def add_channels(self,chans):
+        for chan in chans:
+            if chan not in self.clist:
+                self.clist.append(chan)
+
+
+class PropertyBinning:
+    """
+
+    """
+    def __init__(self, visFr):
+        self.visFr = visFr
+        self.pipeline = visFr.pipeline
+
+        visFr.AddMenuItem('Experimental', 'Bin Event Property by coordinate', self.OnBinProperty,
+                          helpText='Bin average an event property, e.g. background,  as a function of another property, e.g. x')
+
+    def OnBinProperty(self, event=None):
+        # these are the defaults
+        avProperty = 'nPhotons'
+        byProperty = 'x'
+        binwidth = 100
+
+        # traitsui dialog to set the event properties etc
+        pChoice = propertyChoice()
+        pChoice.add_channels(sorted(self.pipeline.keys()))
+        # select defaults if these event properties exist
+        if avProperty in pChoice.clist:
+            pChoice.EventProperty = avProperty
+        if byProperty in pChoice.clist:
+            pChoice.BinByProperty = byProperty
+
+        if pChoice.configure_traits(kind='modal'):
+            avProperty = pChoice.EventProperty
+            byProperty = pChoice.BinByProperty
+            binwidth = pChoice.BinWidth
+        else:
+            return
+
+        avp = self.pipeline[avProperty]
+        byp = self.pipeline[byProperty]
+
+        bypmin = byp.min()
+        bypmax = byp.max()
+        nbins = int((bypmax-bypmin)/binwidth)
+        bins = bypmin + np.arange(nbins+1)*binwidth
+
+        binctrs = 0.5*(bins[0:-1]+bins[1:])
+        
+        avbin, abins = np.histogram(byp, bins=bins, weights=avp)
+        bybin, bybins = np.histogram(byp, bins=bins)
+
+        good = bybin > 0
+        bad = bybin <= 0
+
+        avmean = np.zeros_like(avbin,dtype='float64')
+        avmean[good] = avbin[good]/bybin[good]
+        avmean[bad] = np.nan
+
+        plt.figure()
+        plt.plot(binctrs, avmean)
+        plt.xlabel(byProperty)
+        plt.ylabel('mean of %s' % avProperty)
+
+
+def Plug(visFr):
+    """Plugs this module into the gui"""
+    visFr.propBin = PropertyBinning(visFr)
+
