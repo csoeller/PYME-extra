6c189190551e33cb1a57b7248353f763ec390fb6
Christian Soeller <c.soeller@gmail.com>
2017-11-04 22:07:29 +0000

    mapTools map installation tools

--- PYMEcs/experimental/mapTools.py
+++ PYMEcs/experimental/mapTools.py
@@ -6,15 +6,38 @@ import PYME.Analysis.gen_sCMOS_maps as gmaps
 from PYME.IO.MetaDataHandler import NestedClassMDHandler
 from PYME.IO.image import ImageStack
 from PYME.DSView import ViewIm3D
+from PYME.IO.FileUtils import nameUtils
 
 # import PYMEcs.Analysis.pyNCS.denoisetools as ncs
+
+from traits.api import HasTraits, Str, Int, CStr, List, Enum, Float
+from traitsui.api import View, Item, Group
+from traitsui.menu import OKButton, CancelButton, OKCancelButtons
+
+class cameraChoice(HasTraits):
+    clist = List([])
+    Camera = Enum(values='clist')
+
+    traits_view = View(Group(Item(name = 'Camera'),
+                             label = 'Select Camera',
+                             show_border = True),
+                       buttons = OKCancelButtons)
+
+    def add_cams(self,camlist):
+        for cam in camlist:
+            if cam not in self.clist:
+                self.clist.append(cam)
+
 
 def Warn(parent, message, caption = 'Warning!'):
     dlg = wx.MessageDialog(parent, message, caption, wx.OK | wx.ICON_WARNING)
     dlg.ShowModal()
     dlg.Destroy()
 
-def defaultMapName(source, createPath=False):
+defaultCalibrationDir = nameUtils.getCalibrationDir('',create=False)
+
+# FIXME: needs to learn to deal with flatfield maps
+def defaultMapName(source, createPath=False, calibrationDir=defaultCalibrationDir):
     resname = source.mdh.getOrDefault('Analysis.resultname',None)
     if resname is None:
         return None
@@ -24,13 +47,27 @@ def defaultMapName(source, createPath=False):
     else:
         maptype = 'variance'
 
-    mapname = gmaps.mkDefaultPath(maptype, source.mdh, create=createPath)
+    mapname = gmaps.mkDefaultPath(maptype, source.mdh, create=createPath, calibrationDir=calibrationDir)
     return mapname
-    
-    
+
+# return a list of existing camera directories that have tif files inside (assuming these are camera maps)
+def installedCams(calibrationDir=defaultCalibrationDir):
+    from glob import glob
+    import os
+
+    camdirs = []
+    for (dirpath, dirnames, filenames) in os.walk(calibrationDir):
+        camdirs.extend(dirnames)
+        break
+    fulldirs = [os.path.join(calibrationDir,cdir) for cdir in camdirs]
+    validdirs = [cdir for cdir in fulldirs if glob(os.path.join(cdir, '*.tif'))]
+
+    return validdirs
+
+
 # source passed as PYME ImageStack
-def install_map(source):
-    """Installs a map file to its default location"""
+def install_map(source, calibrationDir=defaultCalibrationDir):
+    """Installs a map file to a calibration directory. By default uses the system claibration directory."""
 
     import os
     if source.mdh.getOrDefault('Analysis.name', '') != 'mean-variance':
@@ -43,11 +80,11 @@ def install_map(source):
                                              source.mdh['Camera.ROIWidth'])
     if not (validROIHeight == source.mdh['Camera.ROIHeight']
             and validROIWidth == source.mdh['Camera.ROIWidth']):
-        msg = 'Partial (ROI based) maps cannot be installed to the default location'
+        msg = 'Partial (ROI based) maps cannot be installed to a calibration directory'
         return msg
 
     if source.mdh.getOrDefault('Analysis.isuniform', False):
-        msg = 'Uniform maps cannot be installed to the default location'
+        msg = 'Uniform maps cannot be installed to ba calibration directory'
         return msg
 
     if source.mdh['Analysis.resultname'] == 'mean':
@@ -55,7 +92,7 @@ def install_map(source):
     else:
         maptype = 'variance'
 
-    mapname = gmaps.mkDefaultPath(maptype, source.mdh, create=True)
+    mapname = gmaps.mkDefaultPath(maptype, source.mdh, create=True, calibrationDir=calibrationDir)
     if os.path.isfile(mapname):
         msg = 'map %s exists, not overwriting' % mapname
         return msg
@@ -63,12 +100,12 @@ def install_map(source):
     source.Save(filename=mapname)
     return None
     
-# attempt to install a map in the default location but check a few things
-# 1) does it have the .tif extension? If not give suitable message and return
+# attempt to install a map in a calibration dir but check a few things
+# 1) does it have the .tif extension?
 # 2) can it be opened as a tiffstack
-# 3) if so pass on to install_map which does additional checks; note that
-#    this function does not overwrite
-def checkAndInstallMap(mapf):
+# 3) if all ok so far pass on to install_map which does additional checks; note that
+#    this function does not overwrite existing maps at the destination
+def checkAndInstallMap(mapf, calibrationDir=defaultCalibrationDir):
     import os
     inst = 0
     
@@ -82,33 +119,34 @@ def checkAndInstallMap(mapf):
         msg = 'asked to install %s, could not open as PYME ImageStack, not a map?' % mapf
         return (inst, msg)
 
-    msg = install_map(source)
+    msg = install_map(source, calibrationDir=calibrationDir)
     if msg is None:
         msg = 'installed map %s in default location' % mapf
-        msg += "\n\t-> %s" % defaultMapName(source)
+        msg += "\n\t-> %s" % defaultMapName(source, calibrationDir=calibrationDir)
         inst = 1
 
     return (inst, msg)
 
+
 # install maps, potentially several
 # if fromfile is a directory attempt to install all maps below that directory
 # if fromfile is a file, attempt to install that single file
-def installMapsFrom(fromfile):
+def installMapsFrom(fromfile, calibrationDir=defaultCalibrationDir):
     from glob import glob
     import os
-
+    
     msgs = []
     ntotal = 0
     if os.path.isdir(fromfile):
         # this is a directory, walk it
-        msgs.append('installing maps from directory %s' % fromfile)
+        msgs.append("Installing maps from directory %s -> System Folder\n" % fromfile)
         mapfiles = [y for x in os.walk(fromfile) for y in glob(os.path.join(x[0], '*.tif'))]
         for mapf in mapfiles:
-            ninst, msg = checkAndInstallMap(mapf)
+            ninst, msg = checkAndInstallMap(mapf, calibrationDir=calibrationDir)
             ntotal += ninst
             msgs.append(msg)
     else:
-        ninst, msg = checkAndInstallMap(fromfile)
+        ninst, msg = checkAndInstallMap(fromfile, calibrationDir=calibrationDir)
         ntotal = 1
         msgs.append(msg)
     msgs.append("\ninstalled %d maps" % ntotal)
@@ -119,7 +157,7 @@ def getInstalledMapList():
     from PYME.IO.FileUtils import nameUtils
     from glob import glob
     import os
-    rootdir = nameUtils.getCalibrationDir('',create=False)
+    rootdir = defaultCalibrationDir
     result = [y for x in os.walk(rootdir) for y in glob(os.path.join(x[0], '*.tif'))]
 
     return result
@@ -147,8 +185,11 @@ GUI class to supply various map tools
                              'List installed maps',
                              self.OnListMaps)
         dsviewer.AddMenuItem('Experimental>Map Tools',
-                             'Install maps from location...',
-                             self.OnInstallMaps)
+                             'Install maps to system calibration directory',
+                             self.OnInstallMapsToSystem)
+        dsviewer.AddMenuItem('Experimental>Map Tools',
+                             'Copy maps from system to user directory',
+                             self.OnCopyMapsToUserDir)
 
         
     def check_mapexists(self, mdh, type = 'dark'):
@@ -235,23 +276,57 @@ GUI class to supply various map tools
       else:
           Warn(None,'no suitable maps found')
 
-
-    def OnInstallMaps(self, event=None):
-        fdialog = wx.DirDialog(None, 'Install maps from ...',
+        
+    def OnInstallMapsToSystem(self, event=None):
+        instmsg = 'Install maps from user directory...'
+        fdialog = wx.DirDialog(None, instmsg,
                                style=wx.DD_DEFAULT_STYLE|wx.DD_DIR_MUST_EXIST|wx.DD_CHANGE_DIR)
 
         if fdialog.ShowModal() == wx.ID_OK:
-            fromfile = fdialog.GetPath().encode()
+            dirSelection = fdialog.GetPath().encode()
+            fdialog.Destroy()
+        else:
+            fdialog.Destroy()
+            return        
+
+        inst, msg = installMapsFrom(dirSelection, calibrationDir=defaultCalibrationDir)
+        
+        dlg = ScrolledMessageDialog(self.dsviewer, msg, instmsg, size=(900,400),
+                                    style=wx.RESIZE_BORDER | wx.DEFAULT_DIALOG_STYLE )
+        dlg.ShowModal()
+        dlg.Destroy()
+
+        
+    def OnCopyMapsToUserDir(self, event=None):
+        import os
+        instmsg = 'Copy maps to user directory...'
+        cdict = {os.path.basename(camdir) : camdir for camdir in installedCams()}
+        cdict['All Cameras'] = defaultCalibrationDir
+        
+        cChoice = cameraChoice()
+        cChoice.add_cams(sorted(cdict.keys()))
+        if not cChoice.configure_traits(kind='modal'):
+            return
+        camdir = cdict[cChoice.Camera]
+
+        fdialog = wx.DirDialog(None, instmsg,
+                               style=wx.DD_DEFAULT_STYLE|wx.DD_DIR_MUST_EXIST|wx.DD_CHANGE_DIR)
+
+        if fdialog.ShowModal() == wx.ID_OK:
+            dirSelection = fdialog.GetPath().encode()
             fdialog.Destroy()
         else:
             fdialog.Destroy()
             return
 
-        inst, msg = installMapsFrom(fromfile)
-        dlg = ScrolledMessageDialog(self.dsviewer, msg, "Installed maps from...", size=(900,400),
-                                      style=wx.RESIZE_BORDER | wx.DEFAULT_DIALOG_STYLE )
+        inst, msg = installMapsFrom(camdir, calibrationDir=dirSelection)
+        
+        dlg = ScrolledMessageDialog(self.dsviewer, msg, instmsg, size=(900,400),
+                                    style=wx.RESIZE_BORDER | wx.DEFAULT_DIALOG_STYLE )
         dlg.ShowModal()
         dlg.Destroy()
+
+    
 
 def Plug(dsviewer):
     dsviewer.mapTool = mapTools(dsviewer)
