8d747f8100fa1da1b55cea58b3b45cfac3e22f08
Christian Soeller <c.soeller@gmail.com>
2018-08-01 16:44:01 +0100

    further event selection tweaking

--- PYMEcs/experimental/randMap.py
+++ PYMEcs/experimental/randMap.py
@@ -1,5 +1,5 @@
 import numpy as np
-from traits.api import HasTraits, Str, Int, CStr, List, Enum, Float
+from traits.api import HasTraits, Str, Int, CStr, List, Enum, Float, Bool
 from traitsui.api import View, Item, Group
 from traitsui.menu import OKButton, CancelButton, OKCancelButtons
 
@@ -23,10 +23,12 @@ class TimeSelector(HasTraits):
     Channel = Enum(values='clist')
     FromTime = Float()
     ToTime = Float()
+    FilterEvents = Bool(False)
 
     traits_view = View(Group(Item(name = 'Channel'),
                              Item(name = 'FromTime'),
-                             Item(name = 'ToTime')),
+                             Item(name = 'ToTime'),
+                             Item(name = 'FilterEvents')),
                        title = 'Select Channel',
                        buttons = OKCancelButtons
     )
@@ -37,8 +39,11 @@ class TimeSelector(HasTraits):
                 self.clist.append(chan)
 
 
+class TimeBlock(HasTraits):
+    BlockSize = Int(100)   
 
-class RandMap:
+
+class ExtraColumns:
     """
 
     """
@@ -51,6 +56,26 @@ class RandMap:
                           self.OnRandMap,
                           helpText='the added random value column can be used to select a fraction of all events')
         visFr.AddMenuItem('Experimental>ExtraColumns',
+                          'Add time block choice column',
+                          self.OnTimeBlocks,
+                          helpText='the added column can be used to select blocks of frames as even or odd numbered blocks')
+        visFr.AddMenuItem('Experimental>ExtraColumns',
+                          'Select 50% of events from even numbered time blocks - EVEN',
+                          self.OnSelectTB1,
+                          helpText='select half of all events for FRC - even numbered time blocks')
+        visFr.AddMenuItem('Experimental>ExtraColumns',
+                          'Select 50% of events from from odd numbered time blocks - ODD',
+                          self.OnSelectTB2,
+                          helpText='select half of all events for FRC - second half')
+        visFr.AddMenuItem('Experimental>ExtraColumns',
+                          'Select 50% of events from random value column - half 1',
+                          self.OnSelectHalf1,
+                          helpText='select half of all events for FRC - first half')
+        visFr.AddMenuItem('Experimental>ExtraColumns',
+                          'Select 50% of events from random value column - half 2',
+                          self.OnSelectHalf2,
+                          helpText='select half of all events for FRC - second half')
+        visFr.AddMenuItem('Experimental>ExtraColumns',
                           'Add channel based time selection column',
                           self.OnTimeSelectChannel,
                           helpText='the added column can be used to select a time window of events of a specific colour, filter tsel_channel in range (0.5,2)')
@@ -62,6 +87,52 @@ class RandMap:
     def OnRandMap(self, event=None):
         self.pipeline.selectedDataSource.setMapping('randVal','0*x+np.random.rand(x.size)')
         self.pipeline.Rebuild()
+
+    def OnTimeBlocks(self, event=None):
+        tb = TimeBlock()
+        if tb.configure_traits(kind='modal'):
+            psd = self.pipeline.selectedDataSource
+            blockSel = np.mod((psd['t']/tb.BlockSize).astype('int'),2)
+            # self.pipeline.selectedDataSource.setMapping('timeBlock',"np.mod((t/%d).astype('int'),2)" % tb.BlockSize)
+            self.pipeline.selectedDataSource.addColumn('timeBlock',blockSel)
+            self.pipeline.Rebuild()
+
+    def OnSelectTB1(self, event=None):
+        if 'timeBlock' not in self.pipeline.keys():
+            print('Need randVal property, call "Add random value column" first')
+            return
+
+        self.pipeline.filterKeys['timeBlock'] = (-0.1,0.5)
+        self.pipeline.Rebuild()
+        self.visFr.CreateFoldPanel()
+
+    def OnSelectTB2(self, event=None):
+        if 'timeBlock' not in self.pipeline.keys():
+            print('Need randVal property, call "Add random value column" first')
+            return
+
+        self.pipeline.filterKeys['timeBlock'] = (0.5,1.5)
+        self.pipeline.Rebuild()
+        self.visFr.CreateFoldPanel()
+
+            
+    def OnSelectHalf1(self, event=None):
+        if 'randVal' not in self.pipeline.keys():
+            print('Need randVal property, call "Add random value column" first')
+            return
+
+        self.pipeline.filterKeys['randVal'] = (-0.1,0.4999)
+        self.pipeline.Rebuild()
+        self.visFr.CreateFoldPanel()
+
+    def OnSelectHalf2(self, event=None):
+        if 'randVal' not in self.pipeline.keys():
+            print('Need randVal property, call "Add random value column" first')
+            return
+
+        self.pipeline.filterKeys['randVal'] = (0.5,1.1)
+        self.pipeline.Rebuild()
+        self.visFr.CreateFoldPanel()
 
     def OnTimeSelectChannel(self, event=None):
         pipeline = self.pipeline
@@ -83,6 +154,8 @@ class RandMap:
             tall[idx] = tsel[idx]
             pipeline.colourFilter.setColour(dispColor)
             pipeline.selectedDataSource.addColumn('tselect_%s' % timeSelector.Channel,tall)
+            if timeSelector.FilterEvents:
+                self.pipeline.filterKeys['tselect_%s' % timeSelector.Channel] = (0.5,2.0)
             pipeline.Rebuild()
             self.visFr.CreateFoldPanel()
 
@@ -112,4 +185,4 @@ class RandMap:
             
 def Plug(visFr):
     '''Plugs this module into the gui'''
-    visFr.randMap = RandMap(visFr)
+    visFr.extraCols = ExtraColumns(visFr)
