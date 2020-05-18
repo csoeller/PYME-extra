a30f9414e34602ee745578698e46acd369a65caa
Christian Soeller <c.soeller@gmail.com>
2020-05-18 18:25:51 +0100

    more event density work

--- PYMEcs/Analysis/eventProperties.py
+++ PYMEcs/Analysis/eventProperties.py
@@ -1,6 +1,8 @@
 import numpy as np
 
+
 # in the below p is supposed to be a pipeline object
+# some heuristics to get the area covered by the pipeline data
 def getarea(p):
     if 'x' in p.filterKeys:
         width = p.filterKeys['x'][1]-p.filterKeys['x'][0]
@@ -10,7 +12,25 @@ def getarea(p):
         height = p.filterKeys['y'][1]-p.filterKeys['y'][0]
     else:
         height = p.mdh.Camera.ROIHeight*p.mdh.voxelsize_nm.y
-    area = 1e-6*width*height
+    area1 = 1e-6*width*height
+
+    nEvents = p['x'].size
+    if nEvents > 1:
+        xrange = [p['x'].min(),p['x'].max()]
+        yrange = [p['y'].min(),p['y'].max()]
+
+        width = xrange[1]-xrange[0]
+        height = yrange[1]-yrange[0]
+
+        area2 = 1e-6*width*height
+
+        if abs(area1-area2)/area1 > 0.2: # we have a > 20 % difference
+            area = area2
+        else:
+            area = area1
+    else:
+        area = area1
+        
     return area # area in um^2
 
 def evtDensity(p):
