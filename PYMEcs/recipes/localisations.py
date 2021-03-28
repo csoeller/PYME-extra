bf0f3241b9c07b939100f261a2eb5875aa126cc3
Christian Soeller <c.soeller@gmail.com>
2021-03-28 21:35:20 +0100

    add BiplanePhotons module

--- PYMEcs/recipes/localisations.py
+++ PYMEcs/recipes/localisations.py
@@ -649,3 +649,106 @@ class TimedSpecies(ModuleBase):
 
         return ts
 
+import wx
+def populate_fresults(fitMod,inp,bgzero=True):
+    r = np.zeros(inp['x'].size, fitMod.fresultdtype)
+    for k in inp.keys():
+        f = k.split('_')
+        if len(f) == 1:
+            try:
+                r[f[0]] = inp[k]
+            except ValueError:
+                pass
+        elif len(f) == 2:
+            try:
+                r[f[0]][f[1]] = inp[k]
+            except ValueError:
+                pass
+        elif len(f) == 3:
+            try:
+                r[f[0]][f[1]][f[2]] = inp[k]
+            except ValueError:
+                pass
+        else:
+            raise RuntimeError('more fields than expected: %d' % len(f))
+
+    if bgzero:
+        r['fitResults']['bg'] = 0
+        r['fitResults']['br'] = 0
+    
+    return r
+
+
+from PYME.IO.MetaDataHandler import NestedClassMDHandler
+def genFitImage(fitMod,fr,mdh,psfname=None):
+    mdh2 = NestedClassMDHandler(mdh)
+    if psfname is not None:
+        mdh2['PSFFile'] = psfname
+    fitim = fitMod.genFitImage(fr,mdh2)
+
+    return fitim
+
+def get_photons(fitim,mdh):
+    nph = fitim.sum()*mdh.getEntry('Camera.ElectronsPerCount')/mdh.getEntry('Camera.TrueEMGain')
+    return nph
+
+
+def nPhotons(fitMod,fr,mdh,psfname=None,nmax=100,progressBar=None,updateStep=100):
+    mdh2 = NestedClassMDHandler(mdh)
+    if psfname is not None:
+        mdh2['PSFFile'] = psfname
+    npoints = min(fr.shape[0],nmax)
+    nph = np.zeros((npoints))
+    us = int(updateStep)
+    for i in range(npoints):
+        nph[i] = get_photons(genFitImage(fitMod,fr[i],mdh2,psfname=None), mdh2)
+        if (progressBar is not None) and ((i % us) == 0):
+            progressBar.Update(100.0*i/float(npoints))
+            wx.Yield()
+    return nph
+
+
+@register_module('BiplanePhotons')
+class BiplanePhotons(ModuleBase):
+
+    inputName = Input('filtered')
+    outputName = Output('withPhotons')
+
+    def execute(self, namespace):
+        inp = namespace[self.inputName]
+        mapped = tabular.mappingFilter(inp)
+
+        fdialog = wx.FileDialog(None, 'Please select PSF to use ...',
+                                #defaultDir=os.path.split(self.image.filename)[0],
+                                wildcard='PSF Files|*.psf|TIFF files|*.tif', style=wx.FD_OPEN)
+        succ = fdialog.ShowModal()
+        if (succ == wx.ID_OK):
+            psfn = filename = fdialog.GetPath()
+           
+            mdh = inp.mdh
+            if mdh.getEntry('Analysis.FitModule') not in ['SplitterFitInterpBNR']:
+                Warn('Plugin works only for Biplane analysis')
+                return
+            fitMod = __import__('PYME.localization.FitFactories.' +
+                                mdh.getEntry('Analysis.FitModule'),
+                                fromlist=['PYME', 'localization', 'FitFactories'])
+
+            fr = populate_fresults(fitMod, inp)
+            progress = wx.ProgressDialog("calculating photon numbers",
+                                         "calculating...", maximum=100, parent=None,
+                                         style=wx.PD_SMOOTH|wx.PD_AUTO_HIDE)
+            nph = nPhotons(fitMod, fr, mdh, psfname=psfn, nmax=1e6,
+                           progressBar=progress, updateStep = 100)
+            progress.Destroy()
+            mapped.addColumn('nPhotons', nph)
+            mapped.addColumn('fitResults_background', inp['fitResults_bg']+inp['fitResults_br'])
+            #mapped.addColumn('sig',float(137.0)+np.zeros_like(inp['x'])) # this one is a straight kludge for mortensenError
+
+
+        # propogate metadata, if present
+        try:
+            mapped.mdh = inp.mdh
+        except AttributeError:
+            pass
+        
+        namespace[self.outputName] = mapped
