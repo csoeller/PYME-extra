9ae37a13a7e4c1061b59b413346c9b633f5f0301
Christian Soeller <c.soeller@gmail.com>
2023-01-09 13:11:20 +0100

    additional MINFLUX analysis functions

--- PYMEcs/experimental/MINFLUX.py
+++ PYMEcs/experimental/MINFLUX.py
@@ -110,6 +110,66 @@ def cornercounts(pipeline,backgroundFraction=0.0):
     ccs = np.array([(1.0-backgroundFraction)*(n_all-(n_2+n_3+n_4)), n_2/2.0, n_3/3.0, n_4/4.0])
     return ccs
 
+def print_basiccornerstats(pipeline, ds='filtered_localizations'):
+    curds = pipeline.selectedDataSourceKey
+    pipeline.selectDataSource(ds)
+    data = pipeline
+    dx_um = (data['x'].max() - data['x'].min())/1e3
+    dy_um = (data['y'].max() - data['y'].min())/1e3
+    area_um2 = dx_um * dy_um
+    print("x extent: %.2f um, y extent: %.2f um, area: %.1f um^2" % (dx_um,dy_um,area_um2))
+    pipeline.selectDataSource('coalesced_nz')
+    from scipy.stats import iqr
+    z_iqr_nm = iqr(data['z'])
+    z_fwhm_nm = z_iqr_nm * 2.35/(2*0.675)
+    z_full_nm = 4.0 * z_iqr_nm
+    print("z extent (IQR): %.1f nm, (FWHM): %.1f nm, (Full extent): %.1f nm" % (z_iqr_nm, z_fwhm_nm, z_full_nm))
+    n1 = data['x'].size
+    pipeline.selectDataSource('closemerged')
+    n2 = data['x'].size
+    print("number corners: %d, (%d closemerged)" % (n1,n2))
+    print("cornerdensity %.1f corners/um^2" % (n1/area_um2))
+    print("cornerdensity %.1f corners/um^2 (closemerged)" % (n2/area_um2))
+    z_fwhm_ratio = z_fwhm_nm / 100.0
+    print("corner volume density: %.1f corners/um^2/100nm" % (n1/area_um2/z_fwhm_ratio))
+    print("corner volume density: %.1f corners/um^2/100nm (closemerged)" % (n2/area_um2/z_fwhm_ratio))
+    pipeline.selectDataSource(curds)
+
+def plot_zextent(pipeline, ds='closemerged', series_name='This series'):
+    
+    def set_axis_style(ax, labels):
+        ax.xaxis.set_tick_params(direction='out')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.set_xticks(np.arange(1, len(labels) + 1), labels=labels)
+        ax.set_xlim(0.25, len(labels) + 0.75)
+        ax.set_xlabel('Sample name')
+
+    fig, ax2 = plt.subplots(1, 1, figsize=(9, 4))
+
+    curds = pipeline.selectedDataSourceKey
+    pipeline.selectDataSource(ds)
+    zdata = pipeline['z']
+    pipeline.selectDataSource(curds)
+    q005,quartile1, medians, quartile3 = np.percentile(zdata, [0.5,25, 50, 75])
+    zdatac = zdata - q005
+
+    ax2.set_title('z - axis value distribution')
+    parts = ax2.violinplot(
+        zdatac, showmeans=True, showmedians=False,
+        showextrema=False, quantiles = [0.25,0.75])
+
+    for pc in parts['bodies']:
+        pc.set_facecolor('#D43F3A')
+        pc.set_edgecolor('black')
+        pc.set_alpha(1)
+
+    # set style for the axes
+    labels = [series_name]
+    set_axis_style(ax2, labels)
+
+    plt.subplots_adjust(bottom=0.15, wspace=0.05)
+
+    
 from scipy.special import binom
 from scipy.optimize import curve_fit
 
@@ -135,18 +195,20 @@ def fourcornerplot(pipeline,sigma=None,backgroundFraction=0.0):
     ax = plt.subplot(111)
     ax.bar(ks-0.4, ccsn, width=0.4, color='b', align='center')
     ax.bar(ks, pnn(ks,popt[0]), width=0.4, color='g', align='center')
+    ax.legend(['Experimental data', 'Fit'])
     print('optimal p: %.3f +- %.3f' % (popt[0],perr[0]))
     p_missed = pn(0,popt[0])
     p_m_min = pn(0,popt[0]+perr[0])
     p_m_max = pn(0,popt[0]-perr[0])
     print('missed fraction: %.2f (%.2f...%.2f)' % (p_missed,p_m_min,p_m_max))
+    return (popt[0],perr[0],pnn(ks,popt[0]),ccsn)
 
 
 sigmaDefault = [0.1,0.1,0.02,0.02]
 backgroundDefault = 0.15
 
 def fourcornerplot_default(pipeline):
-    fourcornerplot(pipeline,sigma=sigmaDefault,backgroundFraction=backgroundDefault)
+    return fourcornerplot(pipeline,sigma=sigmaDefault,backgroundFraction=backgroundDefault)
 
 class MINFLUXanalyser():
     def __init__(self, visFr):
