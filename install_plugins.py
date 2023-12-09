c0d6189ee0ba58e95dbe4a2425c4370650f9d59a
csoeller <c.soeller@gmail.com>
2023-12-09 10:43:35 +0000

    install scripts verbosity tweaks

--- install_plugins.py
+++ install_plugins.py
@@ -6,8 +6,17 @@ import os
 import sys
 from distutils.dir_util import copy_tree
 
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+# this seems needed so that we can actually see the copying messages from copy_tree
+# see also: https://stackoverflow.com/questions/17712722/dir-util-copy-tree-wont-print-the-files-that-it-copies
+
+# note: on some platforms it may use the logging infrastructure, on others the distutils.log framework
+# probably easiest to set both
+import logging
+logging.basicConfig(level=logging.INFO)
+
+from distutils import log
+log.set_verbosity(log.INFO)
+log.set_threshold(log.INFO)
 
 def main():
     this_dir = os.path.dirname(__file__)
@@ -18,7 +27,7 @@ def main():
     except IndexError:  # no argument provided, default to user config directory
         installdir = config.user_config_dir
 
-    eprint("installing plugin files into %s..." % installdir)
+    logging.info("installing plugin files into %s..." % installdir)
     copy_tree(os.path.join(this_dir, 'etc', 'PYME'), installdir, verbose=1)
 
 if __name__ == '__main__':
