dd265fb0d7305fa906c469252e5f0262176a21ec
csoeller <c.soeller@gmail.com>
2024-02-26 10:28:57 +0100

    some joystick tweaks

--- PYMEcs/Acquire/Hardware/Piezos/joystick_c867_digital.py
+++ PYMEcs/Acquire/Hardware/Piezos/joystick_c867_digital.py
@@ -1,14 +1,19 @@
-# a gcspiezo compatible joystick class needs
-# - Enable() and IsEnabled() methods for use by position ui and scope object
-# - an init() method to set the joystick parameters via GCS commands and register the gcspiezo "parent" object
-# - an enablecommands() method that will be used by the gcspiezo "parent" object to enable the joystick
-# - a disablecommands() method that will be used by the gcspiezo "parent" object to disable the joystick
-
+# a piezo_pipython_gcs compatible joystick class needs
+# - Enable() and IsEnabled() methods for use by PYME position ui and scope object
+# - an init() method to set the joystick parameters via GCS commands and register the gcspiezo "parent" instance
+# - an enablecommands() method of GCS commands that will be used by the gcspiezo "parent" object to enable the joystick
+# - a disablecommands() method of GCS commands that will be used by the gcspiezo "parent" object to disable the joystick
+# example usage:
+#    from PYME.Acquire.Hardware.Piezos.piezo_pipython_gcs import GCSPiezoThreaded
+#    from PYMEcs.Acquire.Hardware.Piezos.joystick_c867_digital import digitalJoystick
+#    scope.stage = GCSPiezoThreaded('PI C-867 Piezomotor Controller SN 0122013807', axes=['1', '2'],
+#                                   refmodes='FRF',joystick=digitalJoystick())
 class digitalJoystick:
     def __init__(self):
         self.gcspiezo = None
         self._initialised = False
-        
+
+    # this method needs to be tweaked to the specific controller and joystick model
     def init(self,gcspiezo):
         self.gcspiezo = gcspiezo
         # associate controller axis 1 (1st arg) using velocity control mode (2nd arg, value 3)
@@ -16,6 +21,9 @@ class digitalJoystick:
         self.gcspiezo.pi.gcscommands.HIA(1,3,2,'Axis_1')
         # equivalent for 2nd axis
         self.gcspiezo.pi.gcscommands.HIA(2,3,2,'Axis_2')
+        # set closed loop velocity to a certain value (e.g. 1 meaning 1 mm/s or what?)
+        self.gcspiezo.pi.gcscommands.VEL(self.gcspiezo.axes,1.0)
+
         self._initialised = True
         
     def Enable(self, enabled = True):
@@ -30,13 +38,15 @@ class digitalJoystick:
         self.check_initialised()
         return self.gcspiezo._joystick_enabled
 
-    def enablecommands(self,pidevice): # these methods should only be used from the parent gcspiezo object
+    # GCS commands to enable the joystick
+    def enablecommands(self,pidevice): # this method should only be used from the parent gcspiezo object
+        # possible we should use self.gcspiezo.axes here, e.g. pidevice.gcscommands.HIN(self.gcspiezo.axes,True)
+        pidevice.gcscommands.HIN(self.gcspiezo.axes,True)
+
+    # GCS commands to enable the joystick
+    def disablecommands(self,pidevice): # this method should only be used from the parent gcspiezo object
         # possible we should use self.gcspiezo.axes here
-        pidevice.gcscommands.HIN(['1','2'],True)
-        
-    def disablecommands(self,pidevice): # these methods should only be used from the parent gcspiezo object
-        # possible we should use self.gcspiezo.axes here
-        pidevice.gcscommands.HIN(['1','2'],False)
+        pidevice.gcscommands.HIN(self.gcspiezo.axes,False)
 
     def check_initialised(self):
         if not self._initialised:
