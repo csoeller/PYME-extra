26b4b2375dc48913b20c3d6b3a66a3b1e3f20a14
Christian Soeller <c.soeller@gmail.com>
2026-09-11 20:15:43 +0200

    attempt to go to dynamic versioning from github tags

--- PYMEcs/__init__.py
+++ PYMEcs/__init__.py
@@ -1,1 +1,12 @@
-__version__ = '26.09.02'
+# __version__ = '26.09.02'
+import importlib.metadata
+
+try:
+    # figure out version from installed metadata
+    __version__ = importlib.metadata.version("PYME-extra")
+except importlib.metadata.PackageNotFoundError:
+    # Fallback if the package is not installed (e.g., during local development)
+    try:
+        from ._version import __version__
+    except ImportError:
+        __version__ = "0.0.0.dev0+unknown"
